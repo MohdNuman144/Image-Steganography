379d237d6a1f8fb8d2420344bb0c02598977cdc1
Mohammed Numan <75877766+MohdNuman144@users.noreply.github.com>
2023-06-12 13:29:24 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.ayush.steganography;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button encode = findViewById(R.id.encode_button);
+        Button decode = findViewById(R.id.decode_button);
+
+        encode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Encode.class));
+            }
+        });
+
+        decode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Decode.class));
+            }
+        });
+
+    }
+
+}
